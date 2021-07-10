38956aedafe28abd9248062321ad3b2f5e05bd0c
chao69 <star010631@gmail.com>
2021-07-11 00:45:09 +0800

    feat: define g.gitignore

---
+++ app.js
@@ -0,0 +1,58 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const Restaurant = require('./models/restaurant')
+
+const app = express()
+
+mongoose.connect('mongodb://localhost/restaurant-list', { useNewUrlParser: true, useUnifiedTopology: true })
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
+
+
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: 'hbs' }))
+
+app.set('view engine', 'hbs')
+
+app.use(express.static("public"))
+
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.get('/', (req, res) => {
+  Restaurant.find()
+    .lean()
+    .then(restaurants => res.render('index', { restaurants }))
+    .catch(error => console.error(error))
+})
+
+app.get('/restaurants/add', (req, res) => {
+  res.render('add')
+})
+
+app.post('/restaurants', (req, res) => {
+  const name = req.body.name
+  const name_en = req.body.name_en
+  const category = req.body.category
+  const image = req.body.image
+  const location = req.body.location
+  const phone = req.body.phone
+  const google_map = req.body.google_map
+  const rating = req.body.rating
+  const description = req.body.description
+  
+  return Restaurant.create({ name, name_en, category, image, location, phone, google_map, rating, description })
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
+
+app.listen(3000, () => {
+  console.log('Express is running on http://localhost:3000')
+})
