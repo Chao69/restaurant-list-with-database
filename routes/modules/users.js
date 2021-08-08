8fe7b4429f6f1a588cf7acd7fbe99db9577c9b5f
chao69 <star010631@gmail.com>
2021-08-08 20:12:12 +0800

    feat: add register function

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const User = require('../../models/user')
 
 router.get('/login', (req, res) => {
   res.render('login')
@@ -7,6 +8,34 @@ router.get('/login', (req, res) => {
 
 router.get('/register', (req, res) => {
   res.render('register')
+})
+
+router.post('/register', (req, res) => {
+  const { name, email, password, confirmPassword } = req.body
+  // 透過email找資料庫使否有重複的email
+  User.findOne({ email })
+    .then(user => {
+      // 若有重複，則無法註冊
+      if (user) {
+        console.log('User already exists.')
+        // 將使用者輸入的資料回傳至register page保留
+        res.render('register', {
+          name,
+          email,
+          password,
+          confirmPassword
+        })
+      } else {
+        // 若無重複，則在DB新增一筆使用者資料
+        return User.create({
+          name,
+          email,
+          password
+        })
+        .then(() => res.redirect('/'))
+        .catch(error => console.error(error))
+      }
+    })
 })
 
 module.exports = router
