ab7e6ce5036dddaa90f3fc517a18c9e7fb88e425
chao69 <star010631@gmail.com>
2021-08-08 23:44:31 +0800

    feat: add comment

--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -5,6 +5,7 @@ const list = data.results
 const User = require('../user')
 const bcrypt = require('bcryptjs')
 
+// 種子使用者
 const SEED_USERS = [
   {
     name: 'Eric',
@@ -23,6 +24,7 @@ const SEED_USERS = [
 
 db.once('open', () => {
   Promise.all(Array.from(SEED_USERS, (SEED_USER, i) => {
+    // 先將使用者密碼做雜湊，再透過雜湊過後的密碼丟到資料庫建立資料，以避免使用者密碼後台看得一清二楚
     return bcrypt
       .genSalt(10)
       .then(salt => bcrypt.hash(SEED_USER.password, salt))
@@ -31,9 +33,12 @@ db.once('open', () => {
         email: SEED_USER.email,
         password: hash,
       }))
+      // 取出剛建立的使用者，做擁有餐廳的資料建立
       .then(user => {
         const userId = user.id
+        // 透過filter將使用者擁有的餐廳id跟data餐廳的id對比，相同的抽出。
         const restaurants = data.filter(restaurant => SEED_USER.ownRestaurantsId.includes(restaurant.id))
+        // 透過forEach將抽出的餐廳加上userId綁定該使用者
         restaurants.forEach(restaurant => restaurant.userId = userId)
         return Restaurant.create(restaurants)
       })
@@ -44,18 +49,3 @@ db.once('open', () => {
     })
     .catch(err => console.log(err))
 })
-
-// list.forEach(restaurant => {
-//   Restaurant.create({
-//     id: restaurant.id,
-//     name: restaurant.name,
-//     name_en: restaurant.name_en,
-//     category: restaurant.category,
-//     image: restaurant.image,
-//     location: restaurant.location,
-//     phone: restaurant.phone,
-//     google_map: restaurant.google_map,
-//     rating: restaurant.rating,
-//     description: restaurant.description
-//   })
-// })
